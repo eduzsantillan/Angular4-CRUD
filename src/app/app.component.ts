1de96510bb3bace363fac0202a04656a33bf44cf
eduzsantillan <eduardo.zuniga@hundred.com.pe>
2017-11-06 17:53:29 -0500

    Commit 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import {PostService} from './posts.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PostService]
 })
 export class AppComponent {
-  title = 'app';
+  title : string;
+  employees=[
+    {name:'Eduardo',position:'Manager',email:'aaa@bb.com'},
+    {name:'Ronaldo',position:'Designer',email:'aaa@bb.com'},
+    {name:'Messi',position:'Programmer',email:'aaa@bb.com'},
+  ];
+  model:any={};
+  modelToUpdate:any={};
+  indexToUpdate;
+  msgResult:string;
+  hideShowUpdate:boolean;
+
+  constructor() {
+    this.title='Angular CRUD';
+    this.msgResult='';
+    this.hideShowUpdate=true;
+
+  }
+
+  addEmployee():void{
+    this.employees.push(this.model);
+    this.msgResult='Employee has been created successfully!'
+  }
+
+  deleteEmployee(i):void{
+    var answer= confirm('Are you sure to delete this employee?');
+    //Yes:true
+    //No:false
+
+    if(answer){
+      this.employees.splice(i,1);
+    }
+
+    this.msgResult='Employee has been deleted successfully!';
+  
+  }
+
+  editEmployee(i):void{
+    this.hideShowUpdate=false;
+    this.modelToUpdate.name= this.employees[i].name;
+    this.modelToUpdate.position= this.employees[i].position;
+    this.modelToUpdate.email= this.employees[i].email;
+    this.indexToUpdate=i;
+  }
+
+  updateEmployee():void{
+    let i = this.indexToUpdate;
+    this.employees[i]= this.modelToUpdate;
+    this.modelToUpdate={};
+    this.msgResult='Employee has been updated successfully!'
+  }
+
+  closeAlert():void{
+    this.msgResult='';
+  }
+
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
